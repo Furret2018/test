9ca94593e9cdb566db5106a42a55b2b759390f48
AuraSF <370402166@qq.com>
2024-01-04 22:06:29 +0800

    Update main.c

--- ColourfulConsole/main.c
+++ ColourfulConsole/main.c
@@ -1,9 +1,7 @@
 #include "colourfulconsole.h"
 
 int main(int argc, char *argv[]) {
-	setConsoleColour(0xF4);   // 0xF4 = 011111 0100b = 强白背景 + 红字
 	print(RED, "Hello, Colourful Console! %d", RED);
-	resetConsoleColour();
-	
+
 	return 0;
 }
